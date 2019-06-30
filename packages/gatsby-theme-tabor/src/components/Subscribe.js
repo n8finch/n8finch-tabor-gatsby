5a283237a22fd841a66b910fa741166fa013ffba
Nate Finch <finchproservices@gmail.com>
2019-06-30 01:00:25 -0500

    change and add some styles

--- packages/gatsby-theme-tabor/src/components/Subscribe.js
+++ packages/gatsby-theme-tabor/src/components/Subscribe.js
@@ -5,23 +5,10 @@ const Subscribe = () => (
     <div className="widget-area__inner">
       <div className="widget-area__wrapper">
         <aside className="widget widget_text clearfix">
-          <h6 className="h2">Subscribe Now</h6>
+          <h6 className="h2">Thoughts?</h6>
           <div className="textwidget">
-            <p>
-              This is where you’d add your
-              <a href="http://shareasale.com/r.cfm?b=601672&amp;u=1143612&amp;m=49337&amp;urllink=&amp;afftrack=">
-                OptinMonster
-              </a>
-              , or MailChimp for WordPress widgets. I don’t want to give you the
-              API credentials to my OptinMonster account now do I.{' '}
-              <img
-                draggable="false"
-                className="emoji"
-                alt="🙂"
-                src="https://s.w.org/images/core/emoji/11.2.0/svg/1f642.svg"
-                width="20px"
-                height="20px"
-              />
+            <p className="subscribe-note">
+             I haven't decided what I'd like to do for comments and interaction on this site yet... For now, if you've got a question comment, please reach out to me on <a href="https://twitter.com/n8finch" target="_blank">Twitter</a>, <a href="https://www.linkedin.com/in/natefinch/" target="_blank">LinkedIn</a>, or in the <a href="https://wordpress.slack.com/" target="_blank">WordPress Slack</a> (I'm n8finch most places)... Cheers!
             </p>
           </div>
         </aside>
