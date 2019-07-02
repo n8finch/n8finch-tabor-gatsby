a9792b0d7c5a9bcc1b2dfe9032e247f108c01b30
Nate Finch <finchproservices@gmail.com>
2019-07-01 22:53:00 -0500

    testing the manifest

--- packages/gatsby-theme-tabor/gatsby-config.js
+++ packages/gatsby-theme-tabor/gatsby-config.js
@@ -5,6 +5,20 @@ const autoprefixer = require("autoprefixer")
 module.exports = ({ wordPressUrl }) => ({
   plugins: [
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `All the Things`,
+        short_name: `All the Things`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
       resolve: `gatsby-source-graphql`,
       options: {
         // This type will contain remote schema Query type
@@ -39,5 +53,6 @@ module.exports = ({ wordPressUrl }) => ({
         trackingId: "UA-72004715-1",
       },
     },
+    
   ],
 })
